5270a6a327adedd846c2e2ded10e875795e69274
rohitdrake <pirnagrasishwa@gmail.com>
2017-04-15 19:38:17 +0530

    First and Final Commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const formidable  = require('formidable');
+
+const app = express();
+app.set('port', (process.env.PORT||3000));
+app.listen(app.get('port'), ()=>{
+  console.log("server is listening at "+app.get('port') );
+});
+
+app.use('/',express.static(__dirname+'/public'));
+
+app.post('/uploads', (req, res)=>{
+  let form = new formidable.IncomingForm();
+  form.uploadDir = process.cwd();
+  let name, size;
+  form
+  .on("file", (field, File)=>{
+    req.FILE=File;
+     name=File.name;
+     size=File.size;
+  })
+  .on("end",  ()=>{
+     res.send(`The size of ${name} is ${size} bytes.`);
+     return;
+  })
+  .parse(req);
+});
